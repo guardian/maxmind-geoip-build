4cc5d95a897971f703c4a1d83becfaed1c1fcc29
Philip Wills <philip.wills@guardian.co.uk>
2012-07-11 14:44:10 +0100

    Tabs to spaces

--- project/Build.scala
+++ project/Build.scala
@@ -3,25 +3,25 @@ import Keys._
 
 object GeoIPBuild extends Build {
 
-	override lazy val settings = super.settings ++ Seq(
-		name := "geoip",
+  override lazy val settings = super.settings ++ Seq(
+    name := "geoip",
 
-		organization := "com.maxmind",
+    organization := "com.maxmind",
 
-		version :=  "1.28",
+    version :=  "1.28",
 
-		javaSource in Compile <<= baseDirectory(_ / "source"),
+    javaSource in Compile <<= baseDirectory(_ / "source"),
 
-		publishTo in ThisBuild <<= (version) { version: String =>
-				val publishType = if (version.endsWith("SNAPSHOT")) "snapshots" else "releases"
-				Some(
-						Resolver.file(
-								"guardian github " + publishType,
-								file(System.getProperty("user.home") + "/guardian.github.com/maven/repo-" + publishType)
-						)
-				)
-		}
-	)
+    publishTo in ThisBuild <<= (version) { version: String =>
+        val publishType = if (version.endsWith("SNAPSHOT")) "snapshots" else "releases"
+        Some(
+            Resolver.file(
+                "guardian github " + publishType,
+                file(System.getProperty("user.home") + "/guardian.github.com/maven/repo-" + publishType)
+            )
+        )
+    }
+  )
 
-	lazy val geoip = Project(id = "geoip", base = file("."), settings = Project.defaultSettings ++ settings)
+  lazy val geoip = Project(id = "geoip", base = file("."), settings = Project.defaultSettings ++ settings)
 }
